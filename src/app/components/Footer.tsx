02589e52329fdb590bf0b83ae5dc16c776718952
Lionel Lebon <zenbom@gmail.com>
2024-12-08 18:36:02 +0100

    chore: structure

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,16 +3,17 @@ import * as React from 'react';
 export const Footer = (): React.ReactNode => {
   return (
     <div className="pt-12 text-center text-xs text-gray-900">
-      Fait avec 🤍 par{' '}
-      <a
-        href="mailto:me [AT] lionellebon.eu"
-        title="Contactez-moi!"
-        className="hover:underline text-violet-600"
-      >
-        Lionel Lebon
-      </a>
-      <br />
-      Réalisé avec des technologies libres
+      <p>
+        Fait avec 🤍 par{' '}
+        <a
+          href="mailto:me [AT] lionellebon.eu"
+          title="Contactez-moi!"
+          className="hover:underline text-violet-600"
+        >
+          Lionel Lebon
+        </a>
+      </p>
+      <p>Réalisé avec des technologies libres</p>
     </div>
   );
 };
